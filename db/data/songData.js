932e162ca5a22d86e5878e884ca1da0dbe333ebb
Steven Saunders <steven@supersuperstore.com>
2021-05-21 21:39:18 +0100

    added songData ready for seeding

---
+++ db/data/songData.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    name: 'song1',
+    leadArtist: 'Artist1',
+    contibutingArtists: [],
+    image: '',
+    yearRealeased: 2010,
+    length: 250,
+    audioSrc: 'http://www.hyperion-records.co.uk/audiotest/1%20Sullivan%20The%20Lost%20Chord,%20Seated%20one%20day%20at%20the%20organ.MP3',
+    albumns: [],
+    user: {},
+    comments: [],
+
+  },
+  {
+    name: 'song2',
+    leadArtist: 'Artist2',
+    contibutingArtists: [],
+    albumn: [],
+    image: '',
+    yearRealeased: 2010,
+    length: 250,
+    audioSrc: 'http://www.hyperion-records.co.uk/audiotest/1%20Vaet%20Videns%20Dominus.MP3',
+    albumns: [],
+    user: {},
+    comments: [],
+  }
+]
