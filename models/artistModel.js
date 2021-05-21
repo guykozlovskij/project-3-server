b425a415e52f0d49e7fc5c649c12d81508c10e63
Aliwebs <11ASkhankausar@gmail.com>
2021-05-21 17:23:19 +0100

    added artist model

---
+++ models/artistModel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const artistSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dob: { type: Date },
+  about: { type: String },
+  image: { type: String, default: 'default link' },
+  songs: [{ type: mongoose.Schema.ObjectId, ref: 'Song' }],
+  albums: [{ type: mongoose.Schema.ObjectId, ref: 'Album' }],
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
+})
+
+export default mongoose.model('Artist', artistSchema)
