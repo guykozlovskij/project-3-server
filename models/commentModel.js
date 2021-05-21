85468c95836455d4ff11ef29e45802a7af5cf803
Aliwebs <11ASkhankausar@gmail.com>
2021-05-21 17:24:40 +0100

    added comment model

---
+++ models/commentModel.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+const commentSchema = new mongoose.Schema({
+  username: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  dependencyId: { type: String, required: true }
+})
+export default mongoose.model('Comment', commentSchema)
