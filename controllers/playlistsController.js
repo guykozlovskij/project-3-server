027235a4f6cec584808271ba69d60b20e25d6153
Aliwebs <11ASkhankausar@gmail.com>
2021-05-24 11:28:25 +0100

    monday morning changes in group

---
+++ controllers/playlistsController.js
@@ -0,0 +1,21 @@
+//! Get all playlist
+
+//! get a particular playlist
+
+//! add an playlist
+
+//! edit an playlist
+
+//! add a song to a playlist 
+
+//! delte a playlist 
+
+//! delete a song from playlist
+
+//! delete user from playlist
+
+//! add a comment to playlist
+
+//! edit a comment to playlist
+
+//! delete a comment to playlist
---
+++ controllers/songsController.js
@@ -0,0 +1,17 @@
+//! Get all songs?
+
+//! Get a particular song
+
+//! Get all comments for a particular song
+
+//! POST a song
+
+//! DELETE song
+
+//! Edit Song
+
+//! add a comment to songs
+
+//! edit a comment to songs
+
+//! delete a comment to songs
