734826d961dc3dd5d993b821535324f94b476608
Aliwebs <11ASkhankausar@gmail.com>
2021-05-25 16:04:54 +0100

    added default album cover

--- models/albumModel.js
+++ models/albumModel.js
@@ -5,7 +5,7 @@ import commentSchema from './commentSchema.js'
 const albumSchema = new mongoose.Schema({
   name: { type: String, required: true },
   artists: [{ type: mongoose.Schema.ObjectId, ref: 'Artist' }],
-  image: { type: String, default: 'default link' },
+  image: { type: String, default: 'https://www.pngkit.com/png/full/20-202815_vinyl-record-png-transparent-vinyl-png.png' },
   year: { type: Date, required: true },
   lenght: { type: Number, required: true },
   songs: [{ type: mongoose.Schema.ObjectId, ref: 'Song' }],
