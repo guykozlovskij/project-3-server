c54fa9e8c87739e70b5f7ae6c4ff1d2cec3dbf7a
Aliwebs <11ASkhankausar@gmail.com>
2021-05-25 09:35:05 +0100

    started playlist controller

--- controllers/playlistsController.js
+++ controllers/playlistsController.js
@@ -1,7 +1,27 @@
+import Playlist from '../models/playlistModel.js'
+
 //! Get all playlist
-
+async function playlistIndex(req, res, next) {
+  try {
+    const playlist = await Playlist.find()
+    res.status(200).json(playlist)
+  } catch (err) {
+    next(err)
+  }
+}
 //! get a particular playlist
-
+async function playlist(req, res, next) {
+  try {
+    const { playlistId } = req.params
+    const playlist = await Playlist.findById(playlistId).populate('users').populate('songs')
+    if (!playlist) {
+      return res.status(404).json({ error: { message: 'Unauthorized' } })
+    }
+    res.status(200).json(playlist)
+  } catch (err) {
+    next(err)
+  }
+}
 //! add an playlist
 
 //! edit an playlist
@@ -19,3 +39,8 @@
 //! edit a comment to playlist
 
 //! delete a comment to playlist
+
+export default {
+  playlistIndex,
+  playlist
+}
--- views/router.js
+++ views/router.js
@@ -1,10 +1,11 @@
 import express from 'express'
+
+import secureRoute from '../middleware/secureRoute.js'
+
 import albumsController from '../controllers/albumsController.js'
 import userController from '../controllers/usersController.js'
 import songController from '../controllers/songsController.js'
-import secureRoute from '../middleware/secureRoute.js'
-
-import secureRoute from '../middleware/secureRoute.js'
+import playlistsController from '../controllers/playlistsController.js'
 
 
 const router = express.Router()
@@ -16,29 +17,32 @@ router.route('/login')
   .post(userController.login)
 
 //? Albums
+//! Album routes
 router.route('/albums')
   .get(albumsController.albumIndex)
-  .post(secureRoute,albumsController.add)
+  .post(secureRoute, albumsController.add)
 
 router.route('/albums/:albumId')
   .get(albumsController.album)
-  .put(secureRoute,albumsController.edit)
-  .delete(secureRoute,albumsController.remove)
+  .put(secureRoute, albumsController.edit)
+  .delete(secureRoute, albumsController.remove)
 
+//! Song routes in albums
 router.route('/albums/:albumId/songs')
   .get(albumsController.songs)
-  .post(secureRoute,albumsController.addSong)
+  .post(secureRoute, albumsController.addSong)
 
 router.route('/albums/:albumId/songs/:songId')
-  .delete(secureRoute,albumsController.removeSong)
+  .delete(secureRoute, albumsController.removeSong)
 
+//! Comment routes in albums
 router.route('/albums/:albumId/comments')
   .get(albumsController.comments)
-  .post(secureRoute,albumsController.addComment)
+  .post(secureRoute, albumsController.addComment)
 
 router.route('/albums/:albumId/comments/:commentId')
-  .put(secureRoute,albumsController.editComment)
-  .delete(secureRoute,albumsController.removeComment)
+  .put(secureRoute, albumsController.editComment)
+  .delete(secureRoute, albumsController.removeComment)
 
 //? Songs
 //! Songs routes
@@ -63,7 +67,11 @@ router.route('/songs/:id/comments/:commentId')
   .put(secureRoute, songController.editComment)
   .delete(secureRoute, songController.deleteComment)
 
+//? Playlist
+router.route('/playlist')
+  .get(playlistsController.playlistIndex)
 
-
+router.route('/playlist/:playlistId')
+  .get(playlistsController.playlist)
 export default router
 
