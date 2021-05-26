8d025205184924d4628078ad453b5d419f7de3c8
Aliwebs <11ASkhankausar@gmail.com>
2021-05-26 16:13:11 +0100

    changed image to cover in playlist and album

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  image: { type: String, default: 'urlwithdefaultavatar' },
+  image: { type: String, default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' },
   about: { type: String },
   playlists: [{ type: mongoose.Schema.ObjectId, ref: 'Playlist' }],
   likes: [{ type: String }],
