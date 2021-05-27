1cf99502fb98ce075c362f18e45a95c1050eb3af
Aliwebs <11ASkhankausar@gmail.com>
2021-05-27 11:55:30 +0100

    fixed spelling

--- db/data/albumData.js
+++ db/data/albumData.js
@@ -2,13 +2,13 @@ export default [
   {
     name: 'Album1',
     year: '2010-01-01',
-    lenght: 0,
+    length: 0,
     songs: []
   },
   {
     name: 'Album2',
     year: '2010-01-02',
-    lenght: 0,
+    length: 0,
     songs: []
   }
 ]
