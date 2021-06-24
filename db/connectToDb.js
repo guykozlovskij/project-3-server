bb07b63a27fae0111887066a51cc1255191cb20e
Aliwebs <11ASkhankausar@gmail.com>
2021-06-24 10:45:54 +0100

    bug fix

--- db/connectToDb.js
+++ db/connectToDb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 // import { dbURL } from '../config/environment.js'
 
-const dbURL = process.env.DB_URL
+const dbURL = process.env.DB_URL || process.env.DB_URI
 //! Connecting to the database
 export default function connectToDb() {
 
